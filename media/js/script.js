2509bd113cb89931ca74ac5805cf31fde0858d6f
Terence Smith <terencesmith1@me.com>
2022-08-27 00:33:23 -0400

    created a contacts page using emailjs and updated text content on about page

--- media/js/script.js
+++ media/js/script.js
@@ -1,7 +1,21 @@
- const menuToggle = document.querySelector('.toggle');
-      const showcase = document.querySelector('.showcase');
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
 
-      menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
-        showcase.classList.toggle('active');
-      })
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
+
+function SendMail() {
+  const params = {
+    from_name: document.getElementById("from_name").value,
+    email_id: document.getElementById("email_id").value,
+    phone: document.getElementById("phone").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_vjx6ux8", "template_8dz8dxk", params)
+    .then(function (res) {
+      alert("Successfully Sent! " + res.status);
+    });
+}
